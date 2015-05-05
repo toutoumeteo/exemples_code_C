784f732f4cc2b97d0e2f8c9c43f4027aa74b2c36
Andre Plante <adjplante@ec.gc.ca>
2015-05-04 20:06:32 -0400

    Complete iso_c_binding exemple_string

---
+++ iso_c_binding/exemple_string/c_print_string.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void c_print_string(char *string)
+{
+  printf("In c_print_string, your string is\n");
+  printf("%s\n",string);
+}
--- iso_c_binding/exemple_string/prog_f.f90
+++ iso_c_binding/exemple_string/prog_f.f90
@@ -1,14 +1,32 @@
 program caracter
 
-  use iso_c_binding, only : c_null_char, c_ptr
+  use iso_c_binding, only : c_char, c_null_char, c_ptr
 
   implicit none
 
-  character(len=5) :: nomvar
+  interface
+     subroutine c_print_string(string) bind(c,name='c_print_string')
+       use iso_c_binding, only : c_char
+       character(kind=c_char) :: string(*)
+     end subroutine c_print_string
+  end interface
+
+  character(kind=c_char,len=5) :: nomvar
+  character(kind=c_char,len=5) :: nomvar2
+  character(kind=c_char,len=5) :: nomvar3
 
   nomvar='ABCD'
-  print*,'namvar',nomvar
+  nomvar2='EFGH'
+  nomvar3='IJKL'
+  print*,'nomvar=',nomvar
+  print*,'c_null_char=(',c_null_char,')'
   nomvar=nomvar//c_null_char
-  print*,'namvar',nomvar    
+  print*,'nomvar=',nomvar    
+
+  call c_print_string(c_char_"ANDRE"//c_null_char)
+  call c_print_string(nomvar)
+  call c_print_string(nomvar2//c_null_char)
+  call c_print_string(nomvar3) ! This is an error, no null char at the end!
+  call c_print_string(nomvar3//c_null_char)
 
 end program caracter
