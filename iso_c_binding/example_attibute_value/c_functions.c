f8128459bcd0892b91f12c1b8151b73ab1492915
Andre Plante <adjplante@ec.gc.ca>
2015-05-07 08:00:09 -0400

    Finaliser example_attibute_value

--- iso_c_binding/example_attibute_value/c_functions.c
+++ iso_c_binding/example_attibute_value/c_functions.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 
-void c_sub(int var1, int *var2)
+void c_sub1(int *varp, int varv)
 {
-
-  printf("   In c_sub   var1 =  %d\n",  var1);
-  printf("   In c_sub  &var1 =  %p\n", &var1);
-  printf("   In c_sub   var2 =  %p\n",  var2);
-  printf("   In c_sub  &var2 =  %p\n", &var2);
-  printf("   In c_sub  *var2 =  %d\n", *var2);
-
-
+  printf("\n");
+  printf("   In c_sub1   pointer varp points to address  :  varp =  %p\n",  varp);
+  printf("   In c_sub1   value at that address           : *varp =  %d\n", *varp);
+  printf("   In c_sub1   address of varv is local to s/r : &varv =  %p\n", &varv);
+  printf("   In c_sub1   Value at this address           :  varv =  %d\n",  varv);
+  printf("\n");
 }
+
+void c_sub2(int **varpp, int *varpv)
+{
+  printf("\n");
+  printf("   In c_sub2   pointer to pointer varpp points to address     :   varpp =  %p\n",   varpp);
+  printf("   In c_sub2   address contained at that location is          :  *varpp =  %p\n",  *varpp);
+  printf("   In c_sub2   value at that address is the final distination : **varpp =  %d\n", **varpp);
+  printf("\n");
+  printf("   In c_sub2   pointer varpv points to address                :   varpv =  %p\n",   varpv);
+  printf("   In c_sub2   value at that address is the final distination :  *varpv =  %d\n",  *varpv);
+  printf("\n");
+}
+
--- iso_c_binding/example_attibute_value/prog_f.f90
+++ iso_c_binding/example_attibute_value/prog_f.f90
@@ -1,23 +1,32 @@
 program prog_f
 
-  use iso_c_binding, only : c_int
+  use iso_c_binding, only : c_ptr, c_loc
 
   implicit none
 
   interface
-
-     subroutine c_sub(my_int, my_intp)
+     subroutine c_sub1(my_intp, my_intv) bind(c)
        use iso_c_binding, only : c_int
-       integer(c_int) :: my_int
-       integer(c_int), value :: my_intp)       
-     end subroutine c_sub
+       integer(c_int)        :: my_intp
+       integer(c_int), value :: my_intv
+     end subroutine c_sub1
+
+     subroutine c_sub2(my_intpp, my_intpv) bind(c)
+       use iso_c_binding, only : c_int, c_ptr
+       type(c_ptr)        :: my_intpp
+       type(c_ptr), value :: my_intpv
+     end subroutine c_sub2
 
   end interface
 
-  integer :: var
+  integer, target :: var
+  type (c_ptr) :: varp
 
   var = 2
+  varp = c_loc(var)
   
-  call c_sub(var,var)
+  call c_sub1(var,var)
+
+  call c_sub2(varp,varp)
 
 end program prog_f
