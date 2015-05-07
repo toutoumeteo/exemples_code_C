fd887370384e599dc56bbbacadf2973d3fdf9513
Andre Plante <adjplante@gmail.com>
2015-05-06 22:51:05 -0400

    Improve example of pointer to pointer in function

--- pointer/p2p_in_function.c
+++ pointer/p2p_in_function.c
@@ -2,6 +2,8 @@
 
 void my_func(int **pptr);
 
+int val = 6000;
+
 int main ()
 {
    int  var;
@@ -12,11 +14,19 @@ int main ()
    /* take the address of var */
    ptr = &var;
 
-   printf("Value of var = %d\n", var );
-   printf("Value available at   *ptr = %d\n",   *ptr);
-   printf("Value available at    ptr = %p\n",    ptr);
-
+   printf("\n");
+   printf("Pointer ptr points to var      :   var = %d\n",  var);
+   printf("Value pointed to by ptr        :  *ptr = %d\n", *ptr);
+   printf("Address where ptr points to    :   ptr = %p\n",  ptr);
+   printf("Address of var                 :  &var = %p\n", &var);
+   printf("Address of pointer             :  &ptr = %p\n", &ptr);
+   
+   printf("Passing address of ptr to function my_func\n\n");
    my_func(&ptr);
+   printf("\n");
+   printf("Back in main, value pointed to by ptr is now  :  *ptr = %d\n", *ptr);
+   printf("\n");
+   
 
    return 0;
 }
@@ -24,7 +34,17 @@ int main ()
 void my_func(int **pptr)
 {
   // This function receives a pointer to a pointer.
-   printf("In my_func: Value available at **pptr = %d\n", **pptr);
-   printf("In my_func: Value available at  *pptr = %p\n",  *pptr);
-   printf("In my_func: Value available at   pptr = %p\n",   pptr);
+  printf("   In my_func: receiving address of ptr in pointer to pointer pptr\n");
+  printf("   In my_func: Address in pptr is the one of ptr in main         :   pptr = %p\n",   pptr);
+  printf("   In my_func: Value pointed by pptr is address of var           :  *pptr = %p\n",  *pptr);
+  printf("   In my_func:                                                     **pptr = %d\n", **pptr);
+  printf("\n");
+  printf("   In my_func: We now change the address in *ptr to the one of variable val \n");
+  printf("                  with line of code *pptr = &val;\n");
+  *pptr = &val;
+  printf("   In my_func: Address of val                                    :   &val = %p\n",   &val);
+  printf("   In my_func: Value pointed by pptr is address of val           :  *pptr = %p\n",  *pptr);
+  printf("   In my_func: Address in pptr is still the one of ptr from main :   pptr = %p\n",   pptr);
+  printf("   In my_func: The value of val is                               :    val = %d\n",    val);
+  printf("   In my_func:                                                     **pptr = %d\n", **pptr);
 }
